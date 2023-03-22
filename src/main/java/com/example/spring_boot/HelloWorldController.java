765752c6136b734e6c501f3be62c81338ad22c4c
anastasia <anastasia.miliutina@gmail.com>
2023-03-22 18:00:51 +0200

    CheckHTTPResponse.java

--- src/main/java/com/example/spring_boot/HelloWorldController.java
+++ src/main/java/com/example/spring_boot/HelloWorldController.java
@@ -1,15 +1,17 @@
 package com.example.spring_boot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
 
-    @RequestMapping ("/")
-
-    public String helloWorld(){
-        return "Hello World";
+@GetMapping("/{name}")
+    public String helloWorld(@PathVariable String name){
+        return "Hello" + name;
     }
 
 }
