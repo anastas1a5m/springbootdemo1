0ed54a125e74939b91e16ec315af8da4298a927a
anastasia <anastasia.miliutina@gmail.com>
2023-04-20 18:49:46 +0300

    Added tests

---
+++ src/main/resources/sql/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE author (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+CREATE TABLE book (
+     id SERIAL PRIMARY KEY,
+     title VARCHAR(100) NOT NULL,
+     author_id INTEGER NOT NULL REFERENCES author(id),
+     published_date DATE NOT NULL,
+     price NUMERIC(8,2) NOT NULL
+);
